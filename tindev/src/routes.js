5c77e3044a42520225c6421ba4c53226c6f70126
Andre Tiago dos Santos Silva <andretiagoss@gmail.com>
2019-09-08 23:18:39 -0300

    Commit inicial do mobile

---
+++ tindev/src/routes.js
@@ -0,0 +1,15 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+
+import Login from './pages/Login';
+import Main from './pages/Main';
+
+export default createAppContainer(
+    // createDrawerNavigator = navega entre telas com a criação de menu lateral. 
+    // createMaterialTopNavigator = navega entre telas com abas no top da tela. 
+    // createStackNavigator = navega entre telas com botão Voltar no cabeçalho criado.
+    // createSwitchNavigator = navega entre telas sem nenhum tipo de feedback visual.
+    createSwitchNavigator({
+        Login,
+        Main
+    })
+);
